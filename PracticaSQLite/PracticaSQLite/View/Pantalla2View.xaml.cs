1ca9bfa623764e1b36ed2c9163b6b99267b38bfc
Daniel Chavez <danchavez_hp@hotmail.com>
2020-10-07 12:58:48 -0500

    Cambios Listview

--- PracticaSQLite/PracticaSQLite/View/Pantalla2View.xaml.cs
+++ PracticaSQLite/PracticaSQLite/View/Pantalla2View.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PracticaSQLite.Modelo;
+using PracticaSQLite.Services;
+using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +15,17 @@ namespace PracticaSQLite.View
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class Pantalla2View : ContentPage
     {
-		public Pantalla2View ()
+        public SQLiteConnection conn;
+        public Pantalla2View ()
 		{
 			InitializeComponent ();
-		}
-	}
+            conn = DependencyService.Get<ISQLitePlatform>().GetConnection();
+            conn.CreateTable<Usuario>();
+        }
+
+        public List<Usuario> GetItemsAsync()
+        {
+            return conn.Table<Usuario>().ToList();
+        }
+    }
 }
